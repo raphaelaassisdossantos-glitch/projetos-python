9aa0c6344336cd91f0667dd4fbff742ebb9b89e8
raphaelaassisdossantos-glitch <raphaelaassisdossantos@gmail.com>
2026-05-04 16:07:45 +0000

    v3

---
+++ quizz.py
@@ -0,0 +1,7 @@
+print("Quizz de Conhecimento Esportivo 🎖️")
+print("1- Em que ano o corinthians conqueistou a primeira libertadores? /n a-2010 /n b-2011 /n c-2012 /n d-2013")
+if resposta == "C":
+   print("Você acertou ✅") 
+else:
+   print("Você errou, tente Novamente 😥")
+   
