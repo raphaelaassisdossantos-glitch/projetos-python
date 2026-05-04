cd118511fa0c4d0ead1f7c64be25059772e9b934
raphaelaassisdossantos-glitch <raphaelaassisdossantos@gmail.com>
2026-05-04 09:33:21 -0300

    v1

---
+++ seletor.py
@@ -0,0 +1,8 @@
+#-- Setor de Caracteres --
+Letra = input("Digite a Letra")
+if letra == "A":
+  print("Você escolheu a Letra A")
+elif letra == "B":
+  print("Você escolheu a Letra B")
+else:
+  print("Essaa opção não está Disponível")
