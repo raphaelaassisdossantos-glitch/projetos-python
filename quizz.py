002050d79141f7ec501cb747e91d0af3f09105a1
raphaelaassisdossantos-glitch <raphaelaassisdossantos@gmail.com>
2026-05-04 18:29:31 +0000

    v3

--- quizz.py
+++ quizz.py
@@ -1,7 +1,23 @@
 print("Quizz de Conhecimento Esportivo 🎖️")
-print("1- Em que ano o corinthians conqueistou a primeira libertadores? /n a-2010 /n b-2011 /n c-2012 /n d-2013")
+print("1- Em que ano o corinthians conqueistou a primeira libertadores?  \n a-2010 \n b-2011 \n c-2012 \n d-2013")
+resposta = input("Digite a alternativa Correta !").upper()
 if resposta == "C":
-   print("Você acertou ✅") 
+    print("Você acertou 🙌")
 else:
-   print("Você errou, tente Novamente 😥")
+    print("Você errou, tente novamente 😢")
+
+print("2- Contra qual time o São paulo conquistou o Mundial de Clubes de 2005? \n a-Liverpool \n b-Barcelona \n c- Milan \n d-Real Madri")
+resposta = input("Digite a alternativa Correta !").upper()
+if resposta == "A":
+    print("Você acertou 🙌 ")
+else:
+    print("Você errou, tente novamente 😢")
    
+print("3- Quantos Titulos do Campeonato Brasileiro o São Paulo conquistou de forma consecutiva entre 2006 e 2008 ? \n a-2 \n b-3 \n c-4 \n d-5")
+resposta = input("Digite a alternativa Correta !").upper()
+if resposta == "B":
+    print("Você acertou 🙌")
+else:
+    print("Você errou, tente novamente 😢")
+
+    #Raphaela Assis
