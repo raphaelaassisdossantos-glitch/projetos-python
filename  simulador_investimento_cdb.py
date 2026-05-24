374fb42dba2b6e30b8320106ce5beded6b194788
raphaelaassisdossantos-glitch <raphaelaassisdossantos@gmail.com>
2026-05-24 02:22:30 +0000

    v1

---  simulador_investimento_cdb.py
+++  simulador_investimento_cdb.py
@@ -1,9 +1,15 @@
 #-- Simulador de Investimendo de CDB--
 deposito = float(input("Digite o valor do Investimento:"))
-taxa = float(input(""))
+taxa = float(input("Qual a taxa do CDB em % ? "))
 meses = int(input("Quantos meses vai investir ? "))
+conversao = taxa / 100
+total = 0
+for mes in range(1, meses +1):
+    total = total + deposito
+    total = total + (total * conversao )
+print(F"Ao final do período, você terá:R${total:.2f}")
 
-
+#Raphaela Assis dos Santos 
 
 
 
