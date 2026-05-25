d44e7435136f7f915edd4efc016c353021f09c1d
raphaelaassisdossantos-glitch <raphaelaassisdossantos@gmail.com>
2026-05-25 12:18:25 +0000

    v1

---
+++ cabecalho.py
@@ -0,0 +1,10 @@
+### Definindo e Chamando a Função 
+def exibir_cabecalho():
+    print('=' * 30)
+    print('SISTEMA DE GESTÃO ESCOLAR')
+    print('Versão 1.0 - Desenvolvido no Codespaces')
+    print('=' * 30)
+
+### Chamando a Função
+exibir_cabecalho()
+print('Menu Principal:')
