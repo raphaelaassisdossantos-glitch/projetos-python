4e687e6521e6856b5b1ffcc714564d60be176e3e
raphaelaassisdossantos-glitch <raphaelaassisdossantos@gmail.com>
2026-05-26 01:02:25 +0000

    v1

--- logistica.py
+++ logistica.py
@@ -3,10 +3,10 @@ def cabecalho():
     print("\n" + "=" * 30)
     print('SISTEMA DE LOGISTICA')
 def calcular_frete(peso_carga):
-    if peso <= 20 and  peso >20:
-        return 10.00
+    if peso_carga <= 20 :
+        return peso_carga * 10.00
     else:
-        return 15.00
+        return peso_carga * 15.00
 cabecalho()
 peso_carga = float(input("Digite o Peso da Carga em (kg):"))
 frete = calcular_frete(peso_carga)
